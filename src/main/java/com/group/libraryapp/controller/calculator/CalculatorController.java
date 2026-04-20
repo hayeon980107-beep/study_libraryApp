7f804118d63b42af6407b12138af109cad9cb536
hayeon980107-beep <hayeon980107@gmail.com>
2026-04-20 17:57:24 +0900

    add api basic

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,7 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,10 @@ public class CalculatorController {
     @GetMapping("/add")
     public int addTwoNumbers(CalculatorAddRequest request) {
         return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply")
+    public int multiplyTwoNumber(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
     }
 }
